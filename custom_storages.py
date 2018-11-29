8f218ce869d139f29f537780aa55782328bb453c
caoimhe daly <keve87@hotmail.com>
2018-11-29 12:56:30 +0000

    custon storages

--- custom_storages.py
+++ custom_storages.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
  
  
-class StaticStorage(S3BotoStorage):
-    location = settings.STATICFILES_LOCATION
+
  
  
 class MediaStorage(S3BotoStorage):
