d852d5c0e749a239b1d50d11e7492fb4bdb35a98
caoimhe daly <keve87@hotmail.com>
2018-11-29 12:51:16 +0000

    deploy

--- tattoo_project/settings/base.py
+++ tattoo_project/settings/base.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tattoo-project-keve87.c9users.io' ]
+ALLOWED_HOSTS = ['tattoo-project-keve87.c9users.io', 'keves-tattoo-project.herokuapp.com' ]
 
 
 # Application definition
